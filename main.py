b5ff55a965bce8808c289c482222a4d5a54f48c4
rat <76930631+rat857@users.noreply.github.com>
2022-09-14 12:18:34 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+import ico
+import info
+# import format
+
+import os
+
+if __name__ == "__main__":
+    ico.icon()
+    if os.path.exists("config.yaml"):
+        print(info.read_info())
+    else:
+        info.write()
+    # mation = input("\033[1;35m查询语法:  \033[0m")
+    # format.seach(mation)
+    import format
+    format.out()
