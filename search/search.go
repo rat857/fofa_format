1f0ebbc195f2b0eefe35c004c31ad59a98852ff3
rat <2269264191@qq.com>
2023-08-03 14:40:24 +0800

    Update search.go

--- search/search.go
+++ search/search.go
@@ -20,7 +20,7 @@ func SearchInfo() string {
 func GetAllUrl() []string {
 	page := rod.New().NoDefaultDevice().MustConnect().MustPage(SearchInfo())
 	color.Red("这一步可能会比较久，请耐心等待。。。")
-	//page.MustWindowFullscreen()//最大化窗口
+	page.MustWindowFullscreen()//最大化窗口
 	//下面这个不接受任何返回的目的是等元素加载完毕 ：获取多个元素的方法的名称都以 MustElements 或 Elements 作为前缀。
 	//单元素选择器和多元素选择器之间的一个关键区别是，单元素选择器会等待元素出现。 如果一个多元素选择器没有找到任何东西，他会立即返回一个空列表。
 	color.Red("正在进行第一层，获取country信息")
@@ -42,7 +42,7 @@ func GetAllUrl() []string {
 	var qbase64 = make([]string, 0)
 	for _, countryUrl := range countryList {
 		page := rod.New().NoDefaultDevice().MustConnect().MustPage(countryUrl)
-		//page.MustWindowFullscreen()
+		page.MustWindowFullscreen()
 		page.MustElementX("//*[@id=\"__layout\"]/div/div[2]/div/div[2]/div[1]/div[3]/div[12]/div/div/ul/li[1]/div/a")
 		r_list := page.MustElementsX("//*[@id=\"__layout\"]/div/div[2]/div/div[2]/div[1]/div[3]/div[12]/div/div/ul/li/div/a")
 		for _, i2 := range r_list {
