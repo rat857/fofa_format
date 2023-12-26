d8441168781b5c4758313e0c1287be88f47764b1
rat <2269264191@qq.com>
2023-12-27 01:13:02 +0800

    Update Juhe.go

--- template/Juhe.go
+++ template/Juhe.go
@@ -47,13 +47,26 @@ type Server struct {
 }
 
 type Data struct {
-	Errmsg          string   `json:"errmsg"`
-	Error           bool     `json:"error"`
-	ConsumedFpoint  int      `json:"consumed_fpoint"`
-	RequiredFpoints int      `json:"required_fpoints"`
-	Size            int      `json:"size"`
-	Page            int      `json:"page"`
-	Mode            string   `json:"mode"`
-	Query           string   `json:"query"`
-	Results         []string `json:"results"`
+	Errmsg          string     `json:"errmsg"`
+	Error           bool       `json:"error"`
+	ConsumedFpoint  int        `json:"consumed_fpoint"`
+	RequiredFpoints int        `json:"required_fpoints"`
+	Size            int        `json:"size"`
+	Page            int        `json:"page"`
+	Mode            string     `json:"mode"`
+	Query           string     `json:"query"`
+	Results         [][]string `json:"results"`
+}
+
+type ResultExcel struct {
+	Host           string `excel:"host"`
+	Protocol       string `excel:"protocol"`
+	CountryName    string `excel:"country_Name"`
+	Region         string `excel:"region"`
+	Domain         string `excel:"domain"`
+	OS             string `excel:"os"`
+	Server         string `excel:"server"`
+	Title          string `excel:"title"`
+	Lastupdatetime string `excel:"lastupdatetime"`
+	Cname          string `excel:"cname"`
 }
